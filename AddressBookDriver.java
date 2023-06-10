871cd40e4c57da1d4efad1008cbbd6c9d48b1aa6
Myles Knowlin <mknowlin@lr1038.local>
2023-06-10 11:40:52 -0400

    made input for driver

--- AddressBookDriver.java
+++ AddressBookDriver.java
@@ -1,13 +1,31 @@
 //package addressBook;
 
 import java.util.List;
+import java.util.Scanner;
+import model.AddressBook;
+import model.Contact;
+
 
 public class AddressBookDriver {
     private static AddressBook addressBook;
     public static void main(String[] args){
+        Scanner user = new Scanner(System.in);
+        boolean runProgram = true;
+
         addressBook  = AddressBook.getInstance();
         List<Contact> contactList = addressBook.getContactList();
-        System.out.println(contactList);
 
+        do {
+            String userInput = user.nextLine();
+
+            if(userInput.equals("end")){
+                runProgram = false;
+            } else {
+            System.out.println(userInput);
+            }
+
+        } while (runProgram);
+
+        user.close();
     }
 }
