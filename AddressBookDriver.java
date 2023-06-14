47011c5bce1837d266970f86b256246677c37f0d
Myles Knowlin <mknowlin@lr1038.local>
2023-06-14 19:47:56 -0400

    completed contact creation making contact viewing

--- AddressBookDriver.java
+++ AddressBookDriver.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 
 
 public class AddressBookDriver {
+    private static Scanner user = new Scanner(System.in);
+    private static String userInput;
+
     private static AddressBook addressBook;
     public static void main(String[] args){
-        Scanner user = new Scanner(System.in);
         boolean runProgram = true;
 
         addressBook  = AddressBook.getInstance();
@@ -24,14 +26,14 @@ public class AddressBookDriver {
 
             contactList = addressBook.getContactList();
 
-            String userInput = user.nextLine();
+            userInput = user.nextLine();
 
             if(userInput.equals("end")){
                 runProgram = false;
             }
 
             switch(userInput){
-                case "create": /* Create Contact */;
+                case "create": contactList = createContact(contactList);
                 break;
 
                 case "view": /* View List of Contacts */;
@@ -52,17 +54,119 @@ public class AddressBookDriver {
         user.close();
     }
 
-    public static void createContact(){
+    public static List<Contact> createContact(List<Contact> contactList){
+        Contact contact = new Contact();
+        
+        System.out.println("give first name");
+        userInput = user.nextLine();
+        contact.getName().setFirstName(userInput);
 
-        System.out.println("give a name");
+        System.out.println("give last name");
+        userInput = user.nextLine();
+        contact.getName().setLastName(userInput);
+
+        System.out.println("give phone number");
+        userInput = user.nextLine();
+        contact.setPhoneNumber(userInput);
+
+        System.out.println("give email address");
+        userInput = user.nextLine();
+        contact.setEmail(userInput);
+
+        contact.getAddresses().add(createMailingAddress());
+
+        System.out.println("Mailing address created");
+
+        contact.getAddresses().add(createHomeAddress());
+
+        System.out.println("Home address created");
+
+        contact.getAddresses().add(createWorkAddress());
+
+        System.out.println("Work address created");
+
+        System.out.println("Contact added");
+        contactList.add(contact);
+
+        return contactList;
+    }
+
+    public static Address createMailingAddress(){
+        Address mailingAddress = new Address();
+
+        System.out.println("give zip of mailing address");
+        userInput = user.nextLine();
+        mailingAddress.setZipAddress(userInput);
+
+        System.out.println("give street of mailing address");
+        userInput = user.nextLine();
+        mailingAddress.setStreetAddress(userInput);
+
+        System.out.println("give city of mailing address");
+        userInput = user.nextLine();
+        mailingAddress.setCityAddress(userInput);
+
+        System.out.println("give state of mailing address");
+        userInput = user.nextLine();
+        mailingAddress.setStateAddress(userInput);
+
+        return mailingAddress;
+    }
+
+    public static Address createHomeAddress(){
+        Address homeAddress = new Address();
+
+        System.out.println("give zip of home address");
+        userInput = user.nextLine();
+        homeAddress.setZipAddress(userInput);
+
+        System.out.println("give street of home address");
+        userInput = user.nextLine();
+        homeAddress.setStreetAddress(userInput);
+
+        System.out.println("give city of home address");
+        userInput = user.nextLine();
+        homeAddress.setCityAddress(userInput);
+
+        System.out.println("give state of home address");
+        userInput = user.nextLine();
+        homeAddress.setStateAddress(userInput);
+
+        return homeAddress;
+    }
+
+    public static Address createWorkAddress(){
+        Address workAddress = new Address();
+
+        System.out.println("give zip of work address");
+        userInput = user.nextLine();
+        workAddress.setZipAddress(userInput);
+
+        System.out.println("give street of work address");
+        userInput = user.nextLine();
+        workAddress.setStreetAddress(userInput);
+
+        System.out.println("give city of work address");
+        userInput = user.nextLine();
+        workAddress.setCityAddress(userInput);
+
+        System.out.println("give state of work address");
+        userInput = user.nextLine();
+        workAddress.setStateAddress(userInput);
+
+        return workAddress;
     }
 
     public String viewContact(List<Contact> contactList){
         String listOContacts = "";
+
         for(int i = 0; contactList.size() < 0; i += 1){
-            listOContacts += contactList.get(i).getName().getFirstName();
-            listOContacts += contactList.get(i).getName().getLastName();
+            listOContacts += contactList.get(i).getName().getFirstName() + " ";
+            listOContacts += contactList.get(i).getName().getLastName() + "\n";
         }
+
         return listOContacts;
     }
+
+    
 }
